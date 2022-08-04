988ea5fcf76b1128cb9113a9ccc5f7c3d020dab6
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-08-04 10:22:56 +0200

    added the logic for clicking the percentage button class and clearing the custom perecentage state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [isError, setIsError] = useState(false);
+  const [tipAmount, setTipAmount] = useState(0.0);
+  const [totalAmount, setTotalAmount] = useState(0.0);
+  const [tipPercent, setTipPercent] = useState("");
+  const [customPercent, setCustomPercent] = useState("");
+  const [percentClicked, setPercentClicked] = useState(false);
+  const [percentClickedId, setPercentClickedId] = useState("");
+  const [formData, setFormData] = useState({
+    bill: "",
+    people: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+    setCustomPercent("")
+    setTipPercent(e.currentTarget.value);
+    setPercentClicked(true);
+    setPercentClickedId(e.currentTarget.id);
+  };
+
+  const percentClass = (id: string): string => {
+    return percentClicked && percentClickedId === id
+      ? "percent-clicked tip-percentage"
+      : "tip-percentage";
+  };
+
+  const handleCustomChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPercentClickedId("")
+    setPercentClicked(false)
+    setCustomPercent(e.target.value)
+  }
+
+  const { bill, people } = formData;
+
   return (
     <div className="">
       <div className="brand">
@@ -15,118 +52,92 @@ function App() {
       <div className="container">
         <form action="" className="bill__form">
           <section className="bill__input-form">
-            {/* <div className="form-control">
-              <label htmlFor="bill">Bill</label>
-              <div className="form-input">
-                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="17">
-                  <path
-                    fill="#9EBBBD"
-                    d="M6.016 16.328v-1.464c1.232-.08 2.22-.444 2.964-1.092.744-.648 1.116-1.508 1.116-2.58v-.144c0-.992-.348-1.772-1.044-2.34-.696-.568-1.708-.932-3.036-1.092V4.184c.56.144 1.012.4 1.356.768.344.368.516.816.516 1.344v.288h1.824v-.432c0-.448-.088-.876-.264-1.284a3.783 3.783 0 00-.744-1.116A4.251 4.251 0 007.54 2.9a5.324 5.324 0 00-1.524-.492V.872H4.288V2.36a5.532 5.532 0 00-1.416.324c-.448.168-.84.392-1.176.672-.336.28-.604.616-.804 1.008-.2.392-.3.844-.3 1.356v.144c0 .96.316 1.708.948 2.244.632.536 1.548.884 2.748 1.044v3.912c-.704-.16-1.248-.472-1.632-.936-.384-.464-.576-1.08-.576-1.848v-.288H.256v.576c0 .464.08.924.24 1.38.16.456.404.88.732 1.272.328.392.744.728 1.248 1.008s1.108.476 1.812.588v1.512h1.728zM4.288 7.424c-.688-.128-1.164-.332-1.428-.612-.264-.28-.396-.644-.396-1.092 0-.464.176-.832.528-1.104.352-.272.784-.448 1.296-.528v3.336zm1.728 5.712V9.344c.768.128 1.328.328 1.68.6.352.272.528.688.528 1.248 0 .544-.196.984-.588 1.32-.392.336-.932.544-1.62.624z"
-                  />
-                </svg>
-                <input
-                  type="number"
-                  className="input-field"
-                  name="bill"
-                  id="bill"
-                  min="0"
-                  required
-                />
-              </div>
-            </div> */}
-
             <div className="form-group">
               <label htmlFor="bill">Bill</label>
               <span className="bill"></span>
-                <input
-                  type="number"
-                  min="0"
-                  className="form-group_input"
-                  name="bill"
-                  id="bill"
-                  required
-                />
+              <input
+                type="number"
+                min="0"
+                className="form-group_input"
+                name="bill"
+                id="bill"
+                required
+                onChange={handleChange}
+                value={bill}
+                placeholder="0"
+              />
             </div>
 
             <div className="form-control">
-              <label htmlFor="select-tip">Select Tip %</label>
+              <label htmlFor="tipPercent">Select Tip %</label>
               <div className="form-input_percent">
                 <input
                   type="button"
-                  name="select-tip"
-                  id="select-tip"
-                  value="5%"
-                  className="tip-percentage"
+                  name="tipPercent"
+                  id="tipPercent5"
+                  value="5"
+                  className={percentClass("tipPercent5")}
+                  onClick={handleClick}
                 />
                 <input
                   type="button"
-                  name="select-tip"
-                  id="select-tip"
-                  value="10%"
-                  className="tip-percentage"
+                  name="tipPercent"
+                  id="tipPercent10"
+                  value="10"
+                  className={percentClass("tipPercent10")}
+                  onClick={handleClick}
                 />
                 <input
                   type="button"
-                  name="select-tip"
-                  id="select-tip"
-                  value="15%"
-                  className="tip-percentage"
+                  name="tipPercent"
+                  id="tipPercent15"
+                  value="15"
+                  className={percentClass("tipPercent15")}
+                  onClick={handleClick}
                 />
                 <input
                   type="button"
-                  name="select-tip"
-                  id="select-tip"
-                  value="25%"
-                  className="tip-percentage"
+                  name="tipPercent"
+                  id="tipPercent25"
+                  value="25"
+                  onClick={handleClick}
+                  className={percentClass("tipPercent25")}
                 />
                 <input
                   type="button"
-                  name="select-tip"
-                  id="select-tip"
-                  value="50%"
-                  className="tip-percentage"
+                  name="tipPercent"
+                  id="tipPercent50"
+                  value="50"
+                  onClick={handleClick}
+                  className={percentClass("tipPercent50")}
                 />
                 <input
                   type="number"
-                  name="select-tip-custom"
-                  id="select-tip-custom"
+                  name="customPercent"
+                  id="customPercent"
                   placeholder="Custom"
                   className="tip-percentage-custom"
+                  onChange={handleCustomChange}
+                  value={customPercent}
                 />
               </div>
             </div>
-
-            {/* <div className="form-control">
-              <label htmlFor="number-people">Number of People</label>
-              <div className="form-input">
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="16">
-                  <path
-                    fill="#9EBBBD"
-                    d="M9.573 7.729c.406 0 .784.07 1.126.209.342.14.639.33.881.569.232.227.438.503.614.82a5.1 5.1 0 01.407.949c.097.312.178.654.242 1.016.062.359.105.699.126 1.011.02.307.031.624.031.945 0 .836-.259 1.512-.768 2.01-.504.492-1.17.742-1.98.742H2.748c-.81 0-1.477-.25-1.98-.742C.259 14.76 0 14.084 0 13.248c0-.322.01-.64.032-.945.02-.312.063-.652.126-1.01.063-.363.144-.705.242-1.017.1-.323.238-.643.407-.948.176-.318.382-.594.613-.821.243-.238.54-.43.882-.57.342-.138.72-.208 1.125-.208.16 0 .313.067.61.265.183.123.397.264.636.421.204.134.48.259.822.372.333.11.671.167 1.005.167a3.19 3.19 0 001.006-.167c.341-.113.618-.238.822-.372l.636-.42c.296-.2.45-.266.61-.266zM6.598 0C7.63 0 8.522.38 9.252 1.129s1.1 1.666 1.1 2.724c0 1.06-.37 1.976-1.1 2.725-.73.75-1.623 1.13-2.654 1.13-1.03 0-1.924-.38-2.653-1.13-.73-.749-1.1-1.666-1.1-2.725 0-1.058.37-1.975 1.1-2.724C4.675.379 5.567 0 6.598 0z"
-                  />
-                </svg>
-                <input
-                  type="number"
-                  className="input-field"
-                  name="people"
-                  id="people"
-                  required
-                />
-              </div>
-            </div> */}
 
             <div className="form-group">
               <label htmlFor="bill">Number of People</label>
               <span className="input-error">Can't be zero</span>
               <span className="people"></span>
-                <input
-                  type="number"
-                  min="0"
-                  className="form-group_input"
-                  name="people"
-                  id="people"
-                  required
-                />
+              <input
+                type="number"
+                min="0"
+                className="form-group_input"
+                name="people"
+                id="people"
+                placeholder="0"
+                onChange={handleChange}
+                value={people}
+                required
+              />
             </div>
           </section>
 
@@ -138,7 +149,7 @@ function App() {
               </div>
 
               <div className="bill__summary-amount">
-                <span>$4.27</span>
+                <span>${tipAmount}</span>
               </div>
             </div>
 
@@ -149,7 +160,7 @@ function App() {
               </div>
 
               <div className="bill__summary-amount">
-                <span>$32.79</span>
+                <span>${totalAmount}</span>
               </div>
             </div>
 
